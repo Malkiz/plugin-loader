14630b6fd6aeb29b11cc2115b7656da65ef19093
omri <omri.ey@walkme.com>
2015-05-20 15:44:55 +0300

    initial

---
+++ plugin.js
@@ -0,0 +1,25 @@
+var events = require('events');
+var reqex = require('require-regexp');
+
+module.exports = loadPlugins;
+
+function loadPlugins(names, paths, initFnName, options) {
+	var eventEmitter = new events.EventEmitter();
+	
+	initFnName = initFnName || 'init';
+	var plugins = reqex(names, paths);
+	var pluginNames = Object.keys(plugins);
+	var initializedPlugins = {};
+	
+	pluginNames.filter(function (name) {
+		return plugins[name].hasOwnProperty(initFnName);
+	}).reduce(function (name) {
+		var plugin = plugins[name];
+		initializedPlugins[name] = plugin[initFnName](eventEmitter, options) || plugin;
+	});
+	
+	return {
+		emitter : eventEmitter,
+		plugins : initializedPlugins
+	};
+}
