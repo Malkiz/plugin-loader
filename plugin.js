57b1e3d53b911a64ac45bf26236bf2147ca155f3
omri <omri.ey@walkme.com>
2015-05-21 11:03:14 +0300

    change api

--- plugin.js
+++ plugin.js
@@ -1,25 +1,23 @@
-var events = require('events');
 var reqex = require('require-regexp');
 
 module.exports = loadPlugins;
 
-function loadPlugins(names, paths, initFnName, options) {
-	var eventEmitter = new events.EventEmitter();
-	
-	initFnName = initFnName || 'init';
+function loadPlugins(names, paths, options) {
 	var plugins = reqex(names, paths);
 	var pluginNames = Object.keys(plugins);
 	var initializedPlugins = {};
-	
+
+	var filter = options.filter || function () {return true;};
+	var init = options.init || function () {};
+
 	pluginNames.filter(function (name) {
-		return plugins[name].hasOwnProperty(initFnName);
+		return filter(name, plugins[name]);
 	}).forEach(function (name) {
 		var plugin = plugins[name];
-		initializedPlugins[name] = plugin[initFnName](eventEmitter, options) || plugin;
+		initializedPlugins[name] = init(name, plugin) || plugin;
 	});
 	
 	return {
-		emitter : eventEmitter,
 		plugins : initializedPlugins
 	};
 }
